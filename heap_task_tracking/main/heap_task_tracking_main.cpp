4f453670ebda32c7f6b0082b42994394098f2fb1
warrenlc <warrencrutcher@gmail.com>
2025-04-25 16:36:56 +0200

    snapshot friday. use-static currently broken

--- heap_task_tracking/main/heap_task_tracking_main.cpp
+++ heap_task_tracking/main/heap_task_tracking_main.cpp
@@ -8,18 +8,38 @@
 #define MAX_BLOCK_NUM 20
 #define MAX_DATA_LENGTH 128
 
+#if USE_STATIC
+    #define TASK_SIZE 4096 
+    StaticTask_t task_sensor_temp;
+    StaticTask_t task_sensor_accel;
+    StaticTask_t task_sensor_light;
+    StaticTask_t task_sensor_gps;
+    StaticTask_t task_sensor_rfid;
+    StaticTask_t task_wind_buffer;
+    StaticTask_t task_moisture_buffer;
+    StaticTask_t task_transmit_buffer;
+
+    StackType_t task_temp_stack[TASK_SIZE];
+    StackType_t task_accel_stack[TASK_SIZE];
+    StackType_t task_light_stack[TASK_SIZE];
+    StackType_t task_gps_stack[TASK_SIZE];
+    StackType_t task_rfid_stack[TASK_SIZE];
+    StackType_t task_wind_stack[TASK_SIZE];
+    StackType_t task_moisture_stack[TASK_SIZE];
+    StackType_t task_transmit_stack[TASK_SIZE];
+#endif
 
 static size_t s_prepopulated_num = 0;
 static heap_task_totals_t s_totals_arr[MAX_TASK_NUM];
 static heap_task_block_t s_block_arr[MAX_BLOCK_NUM];
 
-static sensor_t temp_buffer;
-static sensor_t accel_buffer;
-static sensor_t light_buffer;
-static sensor_t gps_buffer;
-static sensor_t rfid_buffer;
-static sensor_t wind_sensor;
-static sensor_t moisture_sensor;
+static sensor_t sensor_temp;
+static sensor_t sensor_accel;
+static sensor_t sensor_light;
+static sensor_t sensor_gps;
+static sensor_t sensor_rfid;
+static sensor_t sensor_wind;
+static sensor_t sensor_moisture;
 
 static void esp_dump_per_task_heap_info(void)
 {
@@ -48,14 +68,23 @@ static void esp_dump_per_task_heap_info(void)
    
 }
 
+static void sema_init(SemaphoreHandle_t *s_lock)
+{
+    *s_lock = xSemaphoreCreateMutex();
+    if (*s_lock == NULL) {
+        ESP_LOGE("sema_init", "Failed to create mutex");
+        abort();
+    }
+}
 
-static void temp_sensor_task(void* arg)     { run_sensor_task(temp_buffer, "TEMP", 1000); } 
-static void accel_sensor_task(void* arg)    { run_sensor_task(accel_buffer, "ACCEL", 10000); }
-static void light_sensor_task(void* arg)    { run_sensor_task(light_buffer, "LIGHT", 1000); }
-static void gps_sensor_task(void* arg)      { run_sensor_task(gps_buffer, "GPS", 1000); }
-static void rfid_sensor_task(void* arg)     { run_sensor_task(rfid_buffer, "RFID", 1000); }
-static void wind_sensor_task(void* arg)     { run_sensor_task(wind_sensor, "WIND", 1000); }
-static void moisture_sensor_task(void* arg) { run_sensor_task(moisture_sensor, "MOISTURE", 1000); }
+/** replace with template or macro */
+static void temp_sensor_task(void* arg)     { measurement_data_t data{}; run_sensor_task(sensor_temp, data, "TEMP", 1000); } 
+static void accel_sensor_task(void* arg)    { measurement_data_t data{}; run_sensor_task(sensor_accel, data, "ACCEL", 5000); }
+static void light_sensor_task(void* arg)    { measurement_data_t data{}; run_sensor_task(sensor_light, data, "LIGHT", 1000); }
+static void gps_sensor_task(void* arg)      { measurement_data_t data{}; run_sensor_task(sensor_gps, data, "GPS", 1000); }
+static void rfid_sensor_task(void* arg)     { measurement_data_t data{}; run_sensor_task(sensor_rfid, data, "RFID", 1000); }
+static void sensor_wind_task(void* arg)     { measurement_data_t data{}; run_sensor_task(sensor_wind, data, "WIND", 1000); }
+static void sensor_moisture_task(void* arg) { measurement_data_t data{}; run_sensor_task(sensor_moisture, data, "MOISTURE", 1000); }
 
 static void transmit_task(void* arg)
 {
@@ -71,22 +100,24 @@ static void transmit_task(void* arg)
                 s_buf.buf.clear();
                 printf("\n");  
             }
-            s_buf.buf = buffer_t();
+            #if !USE_ETL
+                s_buf.buf = buffer_t();
+            #endif
+
             printf("Buffer %s size: %u\n", label, s_buf.buf.size()); 
             xSemaphoreGive(s_buf.s_lock);
         };
 
-        flush_buffer("TEMP", temp_buffer);
-        flush_buffer("ACCEL", accel_buffer);
-        flush_buffer("LIGHT", light_buffer);
-        flush_buffer("GPS", gps_buffer);
-        flush_buffer("RFID", rfid_buffer);
-        flush_buffer("WIND", wind_sensor);
-        flush_buffer("MOISTURE", moisture_sensor);
-
+        flush_buffer("TEMP", sensor_temp);
+        flush_buffer("ACCEL", sensor_accel);
+        flush_buffer("LIGHT", sensor_light);
+        flush_buffer("GPS", sensor_gps);
+        flush_buffer("RFID", sensor_rfid);
+        flush_buffer("WIND", sensor_wind);
+        flush_buffer("MOISTURE", sensor_moisture);
 
         esp_dump_per_task_heap_info();
-        vTaskDelay(pdMS_TO_TICKS(60000)); 
+        vTaskDelay(pdMS_TO_TICKS(10000)); 
     }
 }
 
@@ -96,20 +127,16 @@ static void task_overflow_flush(void *arg)
     {
         if (xSemaphoreTake(overflow_lock, portMAX_DELAY))
         {
-            //printf("overflow size: %zu\n", overflow.size());
-            for (const auto& k: overflow) 
+            for (const auto& k: _buf_overflow) 
             {
                 printf("Overflow: %s\n", k.payload.c_str());
-                overflow.clear();
+                _buf_overflow.clear();
             }
-            //overflow.shrink_to_fit();
-            overflow = 
-            #if USE_ETL
-                buffer_t(MAX_BUFFER_ENTRIES); //std::vector<measurement_data_t>(); /** Force the reallocation */ 
-            #else
-                buffer_t();
+            
+            #if !USE_ETL
+                _buf_overflow = buffer_t();
+                printf("overflow new size, capacity: %u\n", _buf_overflow.size());
             #endif 
-            //printf("overflow size: %zu\n", overflow.size());
             xSemaphoreGive(overflow_lock);
         }
         vTaskDelay(pdMS_TO_TICKS(60000));
@@ -144,43 +171,55 @@ static void task_heap_check(void *arg)
 
 extern "C" void app_main(void)
 {
-
     #if USE_ETL
         printf("USING ETL\n");
-        overflow = buffer_t(MAX_BUFFER_ENTRIES);  
     #else
         printf("USING libstdc++\n");
-        overflow = buffer_t();
     #endif
-    
+
+    #if USE_STATIC
+        printf("USING xTaskCreateStatic...\n"); 
+    #else
+        printf("USING xTaskCreate...\n");
+    #endif
+
+    vTaskDelay(pdMS_TO_TICKS(1000));
+
     static const char *TAG = "app_main";
     vTaskSetThreadLocalStoragePointer(NULL, 0, (void*)TAG);
+    sema_init(&overflow_lock);
 
-    overflow_lock = xSemaphoreCreateMutex();
-    if (overflow_lock == NULL) {
-        ESP_LOGE(TAG, "Failed to create mutex");
-        abort();
-    }
 
-    sensor_init(&temp_buffer, TEMP_SENSOR);
-    sensor_init(&accel_buffer, ACCEL_SENSOR);
-    sensor_init(&light_buffer, LIGHT_SENSOR);
-    sensor_init(&gps_buffer, GPS_SENSOR);
-    sensor_init(&rfid_buffer, RFID_SENSOR);
-    sensor_init(&wind_sensor, WIND_SENSOR);
-    sensor_init(&moisture_sensor, MOISTURE_SENSOR);
+    sensor_init(&sensor_temp, S_TEMP);
+    sensor_init(&sensor_accel, S_ACCEL);
+    sensor_init(&sensor_light, S_LIGHT);
+    sensor_init(&sensor_gps, S_GPS);
+    sensor_init(&sensor_rfid, S_RFID);
+    sensor_init(&sensor_wind, S_WIND);
+    sensor_init(&sensor_moisture, S_MOISTURE);
 
-    xTaskCreatePinnedToCore(temp_sensor_task, "temp_sensor", 12288, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(accel_sensor_task, "accel_sensor", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(light_sensor_task, "light_sensor", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(gps_sensor_task, "gps_sensor", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(rfid_sensor_task, "rfid_sensor", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(wind_sensor_task, "wind_sensor", 4096, NULL, 5, NULL, 0);
-    xTaskCreatePinnedToCore(moisture_sensor_task, "moisture_sensor", 4096, NULL, 5, NULL, 0);
-    
-    xTaskCreatePinnedToCore(transmit_task, "transmit_task", 8192, NULL, 5, NULL, 1);
+    #if USE_STATIC
+        xTaskCreateStaticPinnedToCore(temp_sensor_task, "temp_sensor", TASK_SIZE, NULL, 5, task_temp_stack, &task_sensor_temp, 0);
+        xTaskCreateStaticPinnedToCore(accel_sensor_task, "accel_sensor", TASK_SIZE, NULL, 5, task_accel_stack, &task_sensor_accel, 0);
+        xTaskCreateStaticPinnedToCore(light_sensor_task, "light_sensor", TASK_SIZE, NULL, 5, task_light_stack, &task_sensor_light, 0);
+        xTaskCreateStaticPinnedToCore(gps_sensor_task, "gps_sensor", TASK_SIZE, NULL, 5, task_gps_stack, &task_sensor_gps, 0);
+        xTaskCreateStaticPinnedToCore(rfid_sensor_task, "rfid_sensor", TASK_SIZE, NULL, 5, task_rfid_stack, &task_sensor_rfid, 0);
+        xTaskCreateStaticPinnedToCore(sensor_wind_task, "sensor_wind", TASK_SIZE, NULL, 5, task_wind_stack, &task_wind_buffer, 0);
+        xTaskCreateStaticPinnedToCore(sensor_moisture_task, "sensor_moisture", TASK_SIZE, NULL, 5, task_moisture_stack, &task_moisture_buffer, 0);
+        xTaskCreateStaticPinnedToCore(transmit_task, "transmit_task", 2*TASK_SIZE, NULL, 5, task_transmit_stack, &task_transmit_buffer, 1);
+    #else
+        xTaskCreatePinnedToCore(temp_sensor_task, "temp_sensor", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(accel_sensor_task, "accel_sensor", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(light_sensor_task, "light_sensor", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(gps_sensor_task, "gps_sensor", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(rfid_sensor_task, "rfid_sensor", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(sensor_wind_task, "sensor_wind", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(sensor_moisture_task, "sensor_moisture", 4096, NULL, 5, NULL, 0);
+        xTaskCreatePinnedToCore(transmit_task, "transmit_task", 8192, NULL, 5, NULL, 1);
+    #endif
+
     xTaskCreatePinnedToCore(task_heap_check, "task_heap_check", 4096, NULL, 5, NULL, 1);
-    xTaskCreatePinnedToCore(task_overflow_flush, "task_overflow_flush", 4096, NULL, 5, NULL, 1);
+    xTaskCreatePinnedToCore(task_overflow_flush, "task_overflow_flush", 8192, NULL, 5, NULL, 1);
 }
 
 
--- heap_task_tracking/main/sensor.hpp
+++ heap_task_tracking/main/sensor.hpp
@@ -24,13 +24,13 @@
 
 typedef enum 
 {
-    TEMP_SENSOR = 0,
-    ACCEL_SENSOR,
-    LIGHT_SENSOR,
-    GPS_SENSOR,
-    RFID_SENSOR,
-    WIND_SENSOR,
-    MOISTURE_SENSOR
+    S_TEMP = 0,
+    S_ACCEL,
+    S_LIGHT,
+    S_GPS,
+    S_RFID,
+    S_WIND,
+    S_MOISTURE, 
 }sensor_id_t;
 
 typedef struct 
@@ -42,14 +42,14 @@ typedef struct
 
 #if USE_ETL
     typedef etl::deque<measurement_data_t, MAX_BUFFER_ENTRIES> buffer_t;
+    #define OVERFLOW_ENTRIES 4*MAX_BUFFER_ENTRIES
+    etl::deque<measurement_data_t, OVERFLOW_ENTRIES> _buf_overflow;
 #else
     typedef std::deque<measurement_data_t> buffer_t;
+    buffer_t _buf_overflow;
 #endif
 
-
-buffer_t overflow;
 SemaphoreHandle_t overflow_lock;
-
 
 typedef struct 
 {
@@ -79,57 +79,48 @@ inline string_t generate_fake_payload(const char* prefix, size_t len) {
     return s;
 }
 
-inline void run_sensor_task(sensor_t& s, const char* label, unsigned delay_ms) 
+inline void run_sensor_task(sensor_t& s, measurement_data_t& data, const char* label, unsigned delay_ms) 
 {
     
     while (true) 
     {
-        measurement_data_t data{};
+        printf("starting run_sensor_task\n");
+        
         data.timestamp = time(nullptr); 
         data.payload = generate_fake_payload(label, MAX_PAYLOAD_LENGTH - 1);
         data.sensor_id = static_cast<sensor_id_t>(s.id); 
 
         measurement_data_t data_copy; 
+        
         if (xSemaphoreTake(s.s_lock, portMAX_DELAY)) 
         {
             if (s.buf.size() < MAX_BUFFER_ENTRIES)
             {
-                //printf("WATERMARK: %d\n", uxTaskGetStackHighWaterMark(NULL));
-
                 s.buf.push_back(std::move(data));
-                //printf("Pushing to %s buffer\n", label);
+                printf("Pushing to %s buffer: %s\n", label, data.payload.c_str());
             }
             else
             {
-                ESP_LOGI(label, "Buffer full, pushing to overflow\n");
+                ESP_LOGW(label, "Buffer full, pushing to overflow\n");
                 data_copy = std::move(data); 
             }
-            //ESP_LOGI(label, "about to give the lock\n");
             xSemaphoreGive(s.s_lock);
-            ESP_LOGI(label, "lock given\n"); 
         }
         if (data_copy.payload.length() > 0 && xSemaphoreTake(overflow_lock, portMAX_DELAY)) 
         {
-            ESP_LOGI(label, "Overflow lock taken\n");
             #if USE_ETL
-                if (overflow.size() >= MAX_BUFFER_ENTRIES)
+                if (_buf_overflow.size() >= OVERFLOW_ENTRIES)
                 {
-                    buffer_t newBuffer = buffer_t(MAX_BUFFER_ENTRIES * 2);
-                    for (auto& k : overflow)
-                        newBuffer.push_back(std::move(k)); 
-                    overflow = std::move(newBuffer); //buffer_t(MAX_BUFFER_ENTRIES * 2); 
-                    ESP_LOGI(label, "Overflow buffer resized\n");
-                    ESP_LOGI(label, "New size: %u\n", overflow.size());
+                    _buf_overflow.pop_front();
+                    ESP_LOGW(label, "Overflow full, popping front\n");
                 }
             #endif 
-            overflow.push_back(std::move(data_copy)); 
-            ESP_LOGI(label, "Overflow\n");
-            printf("\nOverflow size: %u\n", overflow.size());
+            _buf_overflow.push_back(std::move(data_copy)); 
+            printf("\nOverflow size: %u\n", _buf_overflow.size());
             xSemaphoreGive(overflow_lock);
         }
 
 
         vTaskDelay(pdMS_TO_TICKS(delay_ms));
-        ESP_LOGI(label, "Delay done\n");
     }
 }
